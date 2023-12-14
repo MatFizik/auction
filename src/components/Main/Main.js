d16fb1e422dff760a706ad700cb5dfd2d3fe3b1f
Axretit <dfw.adilhan@gmail.com>
2023-12-14 23:15:16 +0600

    feat/adaptive main page

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -10,39 +10,6 @@ import ApiService from "../APIService/ApiService";
 import auth from "../Auth";
 import Search from "antd/es/input/Search";
 
-const cards = [
-  {
-    img: "https://top-fon.com/uploads/posts/2021-07/1626839247_60-p-fon-krasno-fioletovii-61.jpg",
-    label: "Кресло",
-    dateOfStart: "20.06.2021",
-    initPrice: "1000",
-  },
-  {
-    img: "https://top-fon.com/uploads/posts/2021-07/1626839247_60-p-fon-krasno-fioletovii-61.jpg",
-    label: "Кресло",
-    dateOfStart: "20.06.2021",
-    initPrice: "1000",
-  },
-  {
-    img: "https://top-fon.com/uploads/posts/2021-07/1626839247_60-p-fon-krasno-fioletovii-61.jpg",
-    label: "Кресло",
-    dateOfStart: "20.06.2021",
-    initPrice: "1000",
-  },
-  {
-    img: "https://top-fon.com/uploads/posts/2021-07/1626839247_60-p-fon-krasno-fioletovii-61.jpg",
-    label: "Кресло",
-    dateOfStart: "20.06.2021",
-    initPrice: "1000",
-  },
-  {
-    img: "https://top-fon.com/uploads/posts/2021-07/1626839247_60-p-fon-krasno-fioletovii-61.jpg",
-    label: "Кресло",
-    dateOfStart: "20.06.2021",
-    initPrice: "1000",
-  },
-];
-
 const Main = () => {
   const isOpen = useModal((state) => state.isOpen);
   const toOpenModal = useModal((state) => state.open);
@@ -51,7 +18,13 @@ const Main = () => {
   const [auctionsLocal, setAuctionsLocal] = useState([]); // [1,2,3,4,5
   const toCloseModal = useModal((state) => state.close);
   const [categories, setCategories] = useState([]); // [1,2,3,4,5
+  const isAuth = auth((state) => state.isAuth);
+  const signout = auth((state) => state.signout);
 
+  const exit = () => {
+    toCloseModal();
+    signout();
+  }
 
   const navigate = useNavigate();
 
@@ -89,13 +62,20 @@ const Main = () => {
           <div className="user_photo"></div>
           <div className="user_name"></div>
         </div>
+        <div className="menu_items flex_row  flex_center">
+          <span className="menu_item"><Link to="/">Главная</Link></span>
+          <span className="menu_item"><Link to="/">Добавить лот</Link></span>
+          <span className="menu_item"><Link to="/">Участник в лотах</Link></span>
+          <span className="menu_item"><Link to="/">Мои лоты</Link></span>
+          <span className="menu_item exit" onClick={exit}>Выйти</span>
+        </div>
         <div className="menu_burger" onClick={toOpenModal}>
           <div className="menu_burger_line"></div>
           <div className="menu_burger_line"></div>
           <div className="menu_burger_line"></div>
         </div>
       </div>
-      <div className="w100">
+      <div className="wrapper m10">
         <div className="section_content">
           <div className="card_wrapp">
             <div>
@@ -104,11 +84,11 @@ const Main = () => {
           </div>
         </div>
       </div>
-      <div className="w100">
+      <div className="wrapper">
         <div className="title_big flex_center">Аукционы</div>
         <div className="section_content">
           {auctionsLocal?.map((card, i) => (
-            <div className="card_wrapper" onClick={()=> navToNext(card)}>
+            <div className="card_wrapper m10" onClick={()=> navToNext(card)}>
               <Card
                 key={i}
                 id={card.id}
